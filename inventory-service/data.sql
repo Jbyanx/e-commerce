5f1d29e9ffea8a25838345eb5317e9ba2d239ead
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-11-06 20:40:42 -0500

    centralizando mysql y rabbit mq

--- inventory-service/data.sql
+++
@@ -1,7 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-
-INSERT INTO inventory_item (id, product_id, available_quantity, price)
-VALUES
-    (gen_random_uuid(), 'P-1001', 20, 15000.00),
-    (gen_random_uuid(), 'P-1002', 10, 35000.00),
-    (gen_random_uuid(), 'P-1003', 5, 82000.00);
