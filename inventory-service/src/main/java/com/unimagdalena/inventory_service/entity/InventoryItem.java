95fd74933f21e2c0e101e074fd28a567f868d66b
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-11-06 21:31:32 -0500

    creando unique constraints para forzar conflicto al intentar re insertar datos

--- inventory-service/src/main/java/com/unimagdalena/inventory_service/entity/InventoryItem.java
+++ inventory-service/src/main/java/com/unimagdalena/inventory_service/entity/InventoryItem.java
@@ -14,10 +14,17 @@ import java.util.UUID;
 @NoArgsConstructor
 @Getter
 @Setter
+@Table(
+        name = "inventory_item",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "product_id")
+        }
+)
 public class InventoryItem {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id; // (UUID)
+    @Column(nullable = false, unique = true)
     private String productId;// (String)
     private int availableQuantity; //(int)
     private BigDecimal price;// (BigDecimal) – para calcular el monto total.
