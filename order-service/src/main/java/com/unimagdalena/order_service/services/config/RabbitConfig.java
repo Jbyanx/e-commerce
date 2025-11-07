c1651fbd88ae9709f1ead7b5129f5b9dc7e5c310
Jbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-11-06 20:41:37 -0500

    usando keys unificadas para la configuracion de rabbit mq

--- order-service/src/main/java/com/unimagdalena/order_service/services/config/RabbitConfig.java
+++ order-service/src/main/java/com/unimagdalena/order_service/services/config/RabbitConfig.java
@@ -1,43 +1,79 @@
 package com.unimagdalena.order_service.services.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static com.unimagdalena.order_service.services.config.RabbitKeys.*;
+
+
 @Configuration
 public class RabbitConfig {
-
-    public static final String ORDER_EXCHANGE = "order.exchange";
-
-    public static final String INVENTORY_QUEUE = "inventory.queue";
-    public static final String PAYMENT_QUEUE = "payment.queue";
-
-    public static final String RESERVE_INVENTORY = "inventory.reserve";
-    public static final String PROCESS_PAYMENT = "payment.process";
 
     @Bean
     public TopicExchange orderExchange() {
         return new TopicExchange(ORDER_EXCHANGE);
     }
 
+    // === INVENTORY ===
     @Bean
-    public Queue inventoryQueue() {
-        return new Queue(INVENTORY_QUEUE, true);
+    public Queue inventoryEventsQueue() {
+        return new Queue(INVENTORY_EVENTS_QUEUE, true);
     }
 
     @Bean
-    public Queue paymentQueue() {
-        return new Queue(PAYMENT_QUEUE, true);
+    public Binding inventoryReservedBinding(TopicExchange orderExchange) {
+        return BindingBuilder.bind(inventoryEventsQueue())
+                .to(orderExchange)
+                .with(INVENTORY_RESERVED);
     }
 
     @Bean
-    public Binding bindInventory() {
-        return BindingBuilder.bind(inventoryQueue()).to(orderExchange()).with(RESERVE_INVENTORY);
+    public Binding inventoryRejectedBinding(TopicExchange orderExchange) {
+        return BindingBuilder.bind(inventoryEventsQueue())
+                .to(orderExchange)
+                .with(INVENTORY_REJECTED);
+    }
+
+    // === PAYMENT ===
+    @Bean
+    public Queue paymentEventsQueue() {
+        return new Queue(PAYMENT_EVENTS_QUEUE, true);
     }
 
     @Bean
-    public Binding bindPayment() {
-        return BindingBuilder.bind(paymentQueue()).to(orderExchange()).with(PROCESS_PAYMENT);
+    public Binding paymentCompletedBinding(TopicExchange orderExchange) {
+        return BindingBuilder.bind(paymentEventsQueue())
+                .to(orderExchange)
+                .with(PAYMENT_COMPLETED);
     }
+
+    @Bean
+    public Queue paymentEventsFailedQueue() {
+        return new Queue(PAYMENT_EVENTS_FAILED_QUEUE, true);
+    }
+
+    @Bean
+    public Binding paymentFailedBinding(TopicExchange orderExchange) {
+        return BindingBuilder.bind(paymentEventsFailedQueue())
+                .to(orderExchange)
+                .with(PAYMENT_FAILED);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
+
 }
-
